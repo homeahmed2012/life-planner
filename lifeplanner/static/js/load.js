b8745876bdc3ce0601e2f26b357255c6ab1cf1ec
Ahmed Mohamed <ahmed_mohamed_3@azhar.edu.eg>
2019-04-13 17:49:59 +0200

    configure gitignore properly

--- lifeplanner/static/js/load.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-        url:"http://localhost:3000/goals",
-        type:"GET",
-        success: function(data, textStatus, jqXHR){
-           for(var i = 0; i < data.length; i++){
-            $('#glist').append('<li>'+data[i].title+'</li>');
-           }
-        }
-});
