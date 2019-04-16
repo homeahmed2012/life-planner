f1531c72bdb3e4c87b104780a49293d4cebbb129
Ahmed Mohamed <ahmed_mohamed_3@azhar.edu.eg>
2019-04-16 02:20:10 +0200

    frontend: update packages and style comments

---
+++ build/precache-manifest.a3c654e538ebf91ab7007b75c52d41a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7554c248543316ba56ba",
+    "url": "./static/static/js/runtime~main.6877baa2.js"
+  },
+  {
+    "revision": "73aa949cb219c4e2117a",
+    "url": "./static/static/js/main.38feb447.chunk.js"
+  },
+  {
+    "revision": "fc05a301f0ba6c7ccfa2",
+    "url": "./static/static/js/2.799cd9ee.chunk.js"
+  },
+  {
+    "revision": "73aa949cb219c4e2117a",
+    "url": "./static/static/css/main.ddd3dd4b.chunk.css"
+  },
+  {
+    "revision": "fc05a301f0ba6c7ccfa2",
+    "url": "./static/static/css/2.58ba0af6.chunk.css"
+  },
+  {
+    "revision": "d9bfe40543cf299ac9dc14ef6adc6cf0",
+    "url": "./static/index.html"
+  }
+];
